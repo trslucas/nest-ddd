b51c92d51a43129b208e2df15fbbd987da4ecc55
Lucas Trindade <lucastrindade@MacBook-Air-de-Lucas.local>
2024-10-28 19:27:03 -0300

    remove unused imports

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,14 +1,8 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
+
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 
 import { PrismaService } from 'src/prisma/prisma.service'
-// import { z } from 'zod'
-
-// const authenticateBodySchema = z.object({
-//   email: z.string().email(),
-//   password: z.string(),
-// })
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
